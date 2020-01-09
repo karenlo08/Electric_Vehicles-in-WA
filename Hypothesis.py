53e63310f061b1dd5e475ef7baf3f4dab586ecbd
karenlo08 <49169212+karenlo08@users.noreply.github.com>
2020-01-08 22:10:44 -0800

    Final Hypothesis
    Finished to encapsulate methods.

--- Hypothesis.py
+++ Hypothesis.py
@@ -6,34 +6,31 @@ import pandas as pd
 import numpy as np
 %matplotlib inline
 
-zip_codes_2017_5yr_commute = pd.read_csv('documents/ev/zip_codes_2017_5yr_commute.csv')
+
+zip_codes_2017_5yr_commute = pd.read_csv('documents/ev2/zip_codes_2017_5yr_commute.csv')
 zip_codes_2017_5yr_commute.rename(columns={"geo_id": "ZIP Code"}, inplace = True)
 
-pop_ev = pd.read_csv('documents/ev/Electric_Vehicle_Population_Data.csv')
+pop_ev = pd.read_csv('documents/ev2/Electric_Vehicle_Population_Data.csv')
 pop_ev_demographics = pd.merge(pop_ev,zip_codes_2017_5yr_commute, on='ZIP Code', how='left')
 
-
-def calculate median_commute():
+def calculate_median_commute():
     median =(zip_codes_2017_5yr_commute['commute_10_14_mins'].apply(lambda x:x * [1] if x>0 else [0]) + 
     zip_codes_2017_5yr_commute['commute_15_19_mins'].apply(lambda x:x * [2] if x>0 else [0]) +
     zip_codes_2017_5yr_commute['commute_20_24_mins'].apply(lambda x:x * [3] if x>0 else [0]) +
     zip_codes_2017_5yr_commute['commute_25_29_mins'].apply(lambda x:x * [4] if x>0 else [0]) +
     zip_codes_2017_5yr_commute['commute_30_34_mins'].apply(lambda x:x * [5] if x>0 else [0]) +     
     zip_codes_2017_5yr_commute['commute_45_59_mins'].apply(lambda x:x * [6] if x>0 else [0]) ).apply(np.mean).astype(int)
-
+    
+    pop_ev_demographics['Median_commute'] = median
     #Reference
-    # commute_dict = {1:'commute_10_14_mins',2:'commute_15_19_mins',3:'commute_20_24_mins',
-    #                 4:'commute_25_29_mins',5:'commute_30_34_mins',6:'commute_45_59_mins'}
-
-    zip_codes_2017_5yr_commute['Median_commute'] = median
-
-
+    #commute_dict = {1:'commute_10_14_mins',2:'commute_15_19_mins',3:'commute_20_24_mins',4:'commute_25_29_mins',5:'commute_30_34_mins',6:'commute_45_59_mins'}
+    
+    
 def decode_make():
     uniq_makes = pop_ev_demographics['Make'].unique()
     make_dict=dict()
     for i in range(len(uniq_makes)):
         make_dict[uniq_makes[i]] =i 
-
     pop_ev_demographics['Make_decode'] = pop_ev_demographics['Make'].map(make_dict)
 
 def columns_to_correlate():
@@ -41,11 +38,13 @@ def columns_to_correlate():
     ev = pd.merge(pop_ev_demographics[['ZIP Code','Make_decode','median_income','Median_commute']],df, on='ZIP Code', how='left')
     ev.rename(columns={0: "Total_EV"}, inplace = True)
     ev = ev[ev['median_income'].notnull()]
+    ev = ev[ev['Median_commute'].notnull()]
+    return ev
 
-def nornmalize():
-    ev2 = ev.copy()
-    cols_to_norm = ['Median_commute','Total_EV']
-    ev2[cols_to_norm] = StandardScaler().fit_transform(ev2[cols_to_norm])
+def normalize(df,column):
+    cols_to_norm = [column,'Total_EV']
+    df[cols_to_norm] = StandardScaler().fit_transform(df[cols_to_norm])
+    return df
 
 
 def pearsonr_ci(x,y,alpha=0.05):
@@ -57,15 +56,25 @@ def pearsonr_ci(x,y,alpha=0.05):
     lo, hi = np.tanh((lo_z, hi_z))
     return r, p, lo, hi
 
-def plot():
+def plot(df):
     corr = ev.corr()
     plt.figure(figsize = (16,4))
-
     ax = sns.heatmap(corr, 
         xticklabels=corr.columns,
         yticklabels=corr.columns,
         annot=True)
 
+def main():
+    calculate_median_commute()
+    decode_make()
+    ev = columns_to_correlate() 
+    Median_commute_norm = normalize(ev,'Median_commute')
+    median_income_norm = normalize(ev,'median_income')
+    pearsonr_ci(Median_commute_norm['Median_commute'].values,Median_commute_norm['Total_EV'].values)
+    pearsonr_ci(median_income_norm['median_income'].values,median_income_norm['Total_EV'].values)
+    plot(ev)
 
-pearsonr_ci(ev2['Median_commute'].values,ev2['Total_EV'].values)
-pearsonr_ci(ev2['median_income'].values,ev2['Total_EV'].values)
+
+if __name__ == '__main__':
+    main()
+
